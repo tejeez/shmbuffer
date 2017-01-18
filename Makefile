bc772ee484624deb75a3f2c80a321c6ca9a65149
Tatu Peltola <tatu.peltola@aalto.fi>
2017-01-18 17:17:17 +0200

    A Couple of bug fixes I had forgotten to commit

--- Makefile
+++ Makefile
@@ -1,8 +1,15 @@
 %: %.c
 	gcc $^ -o $@ -Wall -Wextra -O3 -lrt
 
-all: fixbitorder_shmwrite shmread
+all: fixbitorder_shmwrite shmread shmread_status
 
 fixbitorder_shmwrite: fixbitorder_shmwrite.c
 
 shmread: shmread.c
+
+shmread_status: shmread_status.c
+
+clean:
+	rm fixbitorder_shmwrite shmread shmread_status
+
+.PHONY: all clean
--- shmread_status.c
+++ shmread_status.c
@@ -5,6 +5,7 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <stdlib.h>
 #define BUFS 0x10000
 
 // sizeof(uint16_t)
@@ -15,10 +16,14 @@
 #define SHM_SIZE (DATASIZE*SHM_BUFSIZE + sizeof(size_t))
 
 
-int main() {
+int main(int argc, char *argv[]) {
 	void *shm_buf;
 	size_t nextp = 0, prevp = 0, *shm_p;
 	int shm_fd;
+	long long lagc = 0, lagb = 0, lagn = 0;
+	long long total_read = 0, total_to_read = 0;
+
+	if(argc >= 2) total_to_read = atoll(argv[1]);
 
 	shm_fd = shm_open("/sdr-shm", O_RDONLY, 0644);
 	if(shm_fd < 0) {
@@ -43,6 +48,20 @@ int main() {
 			nwritten = write(1, shm_buf + prevp, nb);
 			prevp += nwritten;
 			assert(prevp <= DATASIZE*SHM_BUFSIZE);
+			
+			lagc += nb;
+			lagb += nwritten;
+			lagn++;
+			if(lagb > 40000000LL) {
+				double lag = (double)lagc / (double)lagn;
+				fprintf(stderr, "Lag: %f bytes\n", lag);
+				lagc = 0;
+				lagb = 0;
+				lagn = 0;
+			}
+
+			total_read += nwritten;
+			if(total_to_read != 0 && total_read >= total_to_read) break;
 		} else if(nextp < prevp) {
 			// wrapped around
 			nb = DATASIZE*SHM_BUFSIZE - prevp;
@@ -53,6 +72,9 @@ int main() {
 				prevp = 0;
 			}
 			assert(prevp <= DATASIZE*SHM_BUFSIZE);
+
+			total_read += nwritten;
+			if(total_to_read != 0 && total_read >= total_to_read) break;
 		} else {
 			// no new data
 			usleep(50000);
